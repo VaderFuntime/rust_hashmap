8d8528e6ef5aa726abc7c6a5705bad32a3512199
Roey Kelner <roeykelner@gmail.com>
2023-02-05 15:06:48 +0200

    "yep"

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,17 @@ struct KeyVal<K: std::hash::Hash + std::cmp::PartialEq, V> {
     val: V,
 }
 
+impl<K: std::hash::Hash + std::cmp::PartialEq, V> KeyVal<K, V> {
+    fn to_tuple(&self) -> (&K, &V) {
+        return (&self.key, &self.val);
+    }
+}
+
 type ElementsVecs<K, V> = Vec<Vec<KeyVal<K, V>>>;
 // A basic hashmap from int to int
 pub struct HashMap<K: std::hash::Hash + std::cmp::PartialEq, V> {
     n_elements: usize,
     element_vecs: ElementsVecs<K, V>,
-    
 }
 
 impl<K: std::hash::Hash + std::cmp::PartialEq, V> Default for HashMap<K, V> {
@@ -26,7 +31,6 @@ impl<K: std::hash::Hash + std::cmp::PartialEq, V> Default for HashMap<K, V> {
         HashMap {
             n_elements: 0,
             element_vecs: HashMap::<K, V>::init_vecs(&INITIAL_CAP),
-            
         }
     }
 }
@@ -57,15 +61,19 @@ impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
         hasher.finish() as usize % self.capacity()
     }
 
-    pub fn capacity(&self) -> usize {
+    fn capacity(&self) -> usize {
         self.element_vecs.len()
+    }
+
+    pub fn contains_key(&self, key : &K) -> bool {
+        self.find(key).is_some()
     }
 
     pub fn size(&self) -> usize {
         self.n_elements
     }
 
-    pub fn at(&self, key: K) -> Option<&V> {
+    pub fn get(&self, key: K) -> Option<&V> {
         if let Some(kv) = self.find(&key) {
             Some(&kv.val)
         } else {
@@ -76,7 +84,7 @@ impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
 
 // adding, removing and rehashing
 impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
-    pub fn put(&mut self, key: K, value: V) {
+    pub fn insert(&mut self, key: K, value: V) {
         if let Some(kv) = self.find_mut(&key) {
             kv.val = value;
             return;
@@ -130,20 +138,19 @@ impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
         self.n_elements = 0;
         for vec in old_element_vecs {
             for kv in vec.into_iter() {
-                self.put(kv.key, kv.val);
+                self.insert(kv.key, kv.val);
             }
         }
     }
 
     fn init_vecs(capacity: &usize) -> ElementsVecs<K, V> {
-        let mut element_vecs: ElementsVecs<K,V> = Vec::new();
+        let mut element_vecs: ElementsVecs<K, V> = Vec::new();
         for _ in 0..*capacity {
             element_vecs.push(Vec::new());
         }
         element_vecs
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -155,37 +162,37 @@ mod tests {
         let mut map = HashMap::new();
         assert_eq!(map.size(), 0);
         assert_eq!(map.capacity(), INITIAL_CAP);
-        map.put(1,2);
+        map.insert(1, 2);
     }
 
     #[test]
     fn add_basic() {
         let mut map = HashMap::new();
         assert_eq!(map.size(), 0);
-        map.put(5, 6);
+        map.insert(5, 6);
         assert_eq!(map.size(), 1);
-        assert_eq!(*map.at(5).unwrap(), 6);
-        assert!(map.at(4).is_none())
+        assert_eq!(*map.get(5).unwrap(), 6);
+        assert!(map.get(4).is_none())
     }
 
     #[test]
     fn add_more() {
         let mut map = HashMap::new();
         for i in 0..20 {
-            map.put(i, i * 3);
+            map.insert(i, i * 3);
         }
         assert_eq!(map.size(), 20);
         for i in 0..20 {
-            assert_eq!(*map.at(i).unwrap(), i * 3);
+            assert_eq!(*map.get(i).unwrap(), i * 3);
         }
         assert_eq!(map.size(), 20);
         for i in 0..20 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), 20);
 
         for i in 30..35 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), 25);
     }
@@ -195,11 +202,11 @@ mod tests {
         let mut map = HashMap::new();
         assert_eq!(map.load_factor(), 0.0);
 
-        map.put(1, 2);
+        map.insert(1, 2);
         assert_eq!(map.load_factor(), 1.0 / INITIAL_CAP as f32);
 
         for i in 0..20 {
-            map.put(i, i * 3);
+            map.insert(i, i * 3);
         }
         assert_eq!(map.size(), 20);
         assert_eq!(map.capacity(), 32);
@@ -209,12 +216,12 @@ mod tests {
     #[test]
     fn test_remove_basic() {
         let mut map = HashMap::new();
-        map.put(5, 6);
+        map.insert(5, 6);
         assert_eq!(map.size(), 1);
         assert_eq!(map.capacity(), INITIAL_CAP);
         map.remove(5);
         assert_eq!(map.size(), 0);
-        assert!(map.at(5).is_none());
+        assert!(map.get(5).is_none());
         assert!(!map.remove(5));
         assert_eq!(map.size(), 0);
     }
@@ -223,13 +230,13 @@ mod tests {
     fn capacity_increase_and_decrease() {
         let mut map = HashMap::new();
         for i in 0..13 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), 13);
         assert_eq!(map.capacity(), 32);
 
         for i in 13..25 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), 25);
         assert_eq!(map.capacity(), 64);
@@ -269,12 +276,12 @@ mod tests {
     fn test_size_is_real() {
         let mut map = HashMap::new();
         for i in 0..13 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), calc_actual_size(&map));
 
         for i in 13..25 {
-            map.put(i, i * 7);
+            map.insert(i, i * 7);
         }
         assert_eq!(map.size(), calc_actual_size(&map));
 
@@ -304,7 +311,7 @@ mod tests {
         assert_eq!(map.size(), calc_actual_size(&map));
     }
 
-    fn calc_actual_size<K: std::hash::Hash + std::cmp::PartialEq ,V>(map: &HashMap<K,V>) -> usize {
+    fn calc_actual_size<K: std::hash::Hash + std::cmp::PartialEq, V>(map: &HashMap<K, V>) -> usize {
         let mut real_count = 0;
 
         for vec in &map.element_vecs {
@@ -317,11 +324,11 @@ mod tests {
     #[test]
     fn test_default_constructor() {
         let mut map = HashMap::new();
-        
+
         assert_eq!(map.size(), 0);
         assert_eq!(map.capacity(), INITIAL_CAP);
-        assert!(map.at(5).is_none());
+        assert!(map.get(5).is_none());
 
-        map.put(1,2);
+        map.insert(1, 2);
     }
 }
