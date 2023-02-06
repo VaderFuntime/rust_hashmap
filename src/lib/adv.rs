26c09e0afddb0e6d825f2a52b64cbd28ec771f4f
Roey Kelner <roeykelner@gmail.com>
2023-02-06 14:09:59 +0200

    changed file structure, added documentation for functions

---
+++ src/lib/adv.rs
@@ -0,0 +1,87 @@
+#![allow(dead_code)]
+
+use super::*;
+
+impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
+
+    /// Inserts the key value pair to the hashmap
+    /// If a pair with this key already exists, overrides the old value
+    pub fn insert(&mut self, key: K, value: V) {
+        if let Some(kv) = self.find_mut(&key) {
+            kv.val = value;
+            return;
+        }
+        let index = self.hash(&key); // TODO why doesn't work withing the line below
+        self.element_vecs[index].push(KeyVal { key, val: value });
+        self.n_elements += 1;
+
+        self.maybe_increase_capacity();
+    }
+
+    // TODO change implementation
+    
+    // pub fn insert_or(&mut self, key: K, value: V) {
+    //     if self.contains_key(&key) {
+    //         return;
+    //     }
+    //     self.insert(key, value);
+    // }
+
+    /// Removes the pair associated with 'key'
+    /// Returns true if was removed, and false if a pair with this key does not exist 
+    pub fn remove(&mut self, key: K) -> bool {
+        let vec_ind = self.hash(&key);
+        let vec = &mut self.element_vecs[vec_ind];
+
+        if let Some(index) = vec.iter().position(|kv| kv.key == key) {
+            vec.remove(index);
+            self.n_elements -= 1;
+            self.maybe_decrease_capacity();
+            return true;
+        }
+
+        false
+    }
+
+    /// Checks whether capacity needs to be decreased, according to the current load factor
+    /// If so, decreases the capacity and re-hashes all pairs
+    fn maybe_decrease_capacity(&mut self) -> bool {
+        if self.load_factor() >= MIN_LOAD_THRESHOLD {
+            return false;
+        }
+
+        self.rehash(std::cmp::max(self.capacity() / 2, MIN_CAPACITY));
+        true
+    }
+
+    /// Checks whether capacity needs to be increased, according to the current load factor
+    /// If so, increases the capacity and re-hashes all pairs
+    fn maybe_increase_capacity(&mut self) -> bool {
+        if self.load_factor() <= MAX_LOAD_THRESHOLD {
+            return false;
+        }
+        self.rehash(self.capacity() * 2);
+        true
+    }
+
+    /// Re-hashes all KeyValue pairs to new vectors, with the given 'new_capacity'
+    fn rehash(&mut self, new_capacity: usize) {
+        if new_capacity == self.capacity() {
+            return;
+        }
+        let old_element_vecs = std::mem::replace(
+            &mut self.element_vecs,
+            HashMap::<K, V>::init_vecs(&new_capacity),
+        );
+        self.n_elements = 0;
+        for vec in old_element_vecs {
+            for kv in vec.into_iter() {
+                self.insert(kv.key, kv.val);
+            }
+        }
+    }
+
+
+}
+
+
---
+++ src/lib/hashmap.rs
@@ -0,0 +1,104 @@
+#![allow(dead_code)]
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
+
+pub mod adv;
+mod tests;
+
+const INITIAL_CAP: usize = 16;
+const MAX_LOAD_THRESHOLD: f32 = 0.75;
+const MIN_LOAD_THRESHOLD: f32 = 0.25;
+const MIN_CAPACITY: usize = 4;
+
+struct KeyVal<K: std::hash::Hash + std::cmp::PartialEq, V> {
+    key: K,
+    val: V,
+}
+
+impl<K: std::hash::Hash + std::cmp::PartialEq, V> KeyVal<K, V> {
+    fn to_tuple(&self) -> (&K, &V) {
+        return (&self.key, &self.val);
+    }
+}
+
+type ElementsVecs<K, V> = Vec<Vec<KeyVal<K, V>>>;
+// A basic hashmap from int to int
+pub struct HashMap<K: std::hash::Hash + std::cmp::PartialEq, V> {
+    n_elements: usize,
+    element_vecs: ElementsVecs<K, V>,
+}
+
+impl<K: std::hash::Hash + std::cmp::PartialEq, V> Default for HashMap<K, V> {
+    fn default() -> Self {
+        HashMap {
+            n_elements: 0,
+            element_vecs: HashMap::<K, V>::init_vecs(&INITIAL_CAP),
+        }
+    }
+}
+
+// basic functions
+impl<K: std::hash::Hash + std::cmp::PartialEq, V> HashMap<K, V> {
+    /// Returns a vector of KeyValue vector of size capacity
+    fn init_vecs(capacity: &usize) -> ElementsVecs<K, V> {
+        let mut element_vecs: ElementsVecs<K, V> = Vec::new();
+        for _ in 0..*capacity {
+            element_vecs.push(Vec::new());
+        }
+        element_vecs
+    }
+
+    /// Returns the current load factor of the hashmap, i.e num_items/capacity
+    fn load_factor(&self) -> f32 {
+        self.size() as f32 / self.capacity() as f32
+    }
+
+    /// Returns a new empty hashmap
+    pub fn new() -> Self {
+        HashMap::default()
+    }
+
+    /// Searches for a given key, and return an optional reference to the KeyValue pair
+    fn find_mut(&mut self, key: &K) -> Option<&mut KeyVal<K, V>> {
+        let index = self.hash(key);
+        self.element_vecs[index].iter_mut().find(|x| x.key == *key)
+    }
+
+    ///
+    fn find(&self, key: &K) -> Option<&KeyVal<K, V>> {
+        let index = self.hash(key);
+        self.element_vecs[index].iter().find(|x| x.key == *key)
+    }
+
+    /// Returns the hash value of the given key, as a function of the current capacity
+    fn hash(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        hasher.finish() as usize % self.capacity()
+    }
+
+    /// Returns the current capacity
+    fn capacity(&self) -> usize {
+        self.element_vecs.len()
+    }
+
+    /// Returns true if the hashmap contains a pair with the given key, and false otherwise
+    pub fn contains_key(&self, key: &K) -> bool {
+        self.find(key).is_some()
+    }
+
+    /// Returns the number of items currently in the hashmap
+    pub fn size(&self) -> usize {
+        self.n_elements
+    }
+
+    /// Return the value associated with the given key, if exists
+    pub fn get(&self, key: &K) -> Option<&V> {
+        if let Some(kv) = self.find(&key) {
+            Some(&kv.val)
+        } else {
+            None
+        }
+    }
+}
